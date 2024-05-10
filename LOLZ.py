cad1075766fe5669e32d52418a582e00a89cadb0
Kwae Htoo <116234889+verylemons@users.noreply.github.com>
2024-05-10 00:31:03 -0700

    Update LOLZ.py

--- LOLZ.py
+++ LOLZ.py
@@ -41,7 +41,7 @@ def demoTAB():
         FRAME_WINDOW = st.image([])
         camera = cv2.VideoCapture(0)
         while run:
-            _, frame = camera.read()
+            ret, frame = camera.read()
             frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
             results = detect_objects(frame)
             FRAME_WINDOW.image(np.array(results.render()))
