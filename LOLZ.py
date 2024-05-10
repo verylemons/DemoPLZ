4567c23c3885471f856f0e194e270abe4bbd2e4a
Kwae Htoo <116234889+verylemons@users.noreply.github.com>
2024-05-09 23:10:05 -0700

    Update LOLZ.py

--- LOLZ.py
+++ LOLZ.py
@@ -1,7 +1,3 @@
-!pip install torch==1.8.1+cu111 torchvision==0.9.1+cu111 torchaudio===0.8.1 -f https://download.pytorch.org/whl/lts/1.8/torch_lts.html
-!cd yolov5 & pip install -r requirements.txt
-
-
 import streamlit as st
 import torch
 from matplotlib import pyplot as plt
